9dbd6d6b926332a1b41a736dff3a669fe17dd410
壮 苗 <doublezhuang101@hotmail.com>
2019-04-23 14:51:41 +0800

    ..

--- GuiApplication/GuiApplication.cpp
+++ GuiApplication/GuiApplication.cpp
@@ -58,9 +58,18 @@ void GuiApplication::connectMysql()
 		QSqlQueryModel *model = new QSqlQueryModel();
 		model->setQuery("SELECT * FROM `password`.`studentinformation` LIMIT 0,1000");
 		ui.tableView->setModel(model);
+		QSqlQuery query("show tables from `password`.`studentinformation`");
+		while (query.next())
+		{
+			qDebug() << query.value(0).toString();
+		}
+		QSqlQuery query1("select *from `password`.`studentinformation`");
+		while (query1.next())
+		{
+			qDebug() << query1.value(0).toString() << query1.value(1).toString();
+		}
 		db.close();
 	}
-
 	else
 	{
 		qDebug() << "fail";
