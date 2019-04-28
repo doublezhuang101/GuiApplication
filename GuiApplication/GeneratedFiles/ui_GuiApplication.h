ae5961ac2ac0252033025c41523ef177435fe157
Sheldon Lee <liwentao0523@hotmail.com>
2019-04-28 09:44:49 +0800

    upload PHP https://nichijou-lab.com/doublezhuang/chart.php

--- GuiApplication/GeneratedFiles/ui_GuiApplication.h
+++ GuiApplication/GeneratedFiles/ui_GuiApplication.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'GuiApplication.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.2
+** Created by: Qt User Interface Compiler version 5.11.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -33,12 +33,12 @@ public:
     void setupUi(QMainWindow *GuiApplicationClass)
     {
         if (GuiApplicationClass->objectName().isEmpty())
-            GuiApplicationClass->setObjectName(QString::fromUtf8("GuiApplicationClass"));
+            GuiApplicationClass->setObjectName(QStringLiteral("GuiApplicationClass"));
         GuiApplicationClass->resize(910, 581);
         centralWidget = new QWidget(GuiApplicationClass);
-        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
         startButton = new QPushButton(centralWidget);
-        startButton->setObjectName(QString::fromUtf8("startButton"));
+        startButton->setObjectName(QStringLiteral("startButton"));
         startButton->setGeometry(QRect(460, 450, 121, 31));
         startButton->setStyleSheet(QString::fromUtf8("font: 13pt \"\346\245\267\344\275\223\";\n"
 "background-color:white;\n"
@@ -50,12 +50,12 @@ public:
 "background-color:rgb(85, 170, 255);\n"
 "border-style: inset;"));
         label = new QLabel(centralWidget);
-        label->setObjectName(QString::fromUtf8("label"));
+        label->setObjectName(QStringLiteral("label"));
         label->setGeometry(QRect(-20, -10, 941, 561));
         label->setPixmap(QPixmap(QString::fromUtf8(":/GuiApplication/MFC ODBC.png")));
         label->setScaledContents(true);
         closeButton = new QPushButton(centralWidget);
-        closeButton->setObjectName(QString::fromUtf8("closeButton"));
+        closeButton->setObjectName(QStringLiteral("closeButton"));
         closeButton->setGeometry(QRect(620, 450, 81, 31));
         closeButton->setStyleSheet(QString::fromUtf8("font: 13pt \"\346\245\267\344\275\223\";\n"
 "background-color:white;\n"
@@ -71,11 +71,11 @@ public:
         startButton->raise();
         closeButton->raise();
         menuBar = new QMenuBar(GuiApplicationClass);
-        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setObjectName(QStringLiteral("menuBar"));
         menuBar->setGeometry(QRect(0, 0, 910, 23));
         GuiApplicationClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(GuiApplicationClass);
-        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
         GuiApplicationClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
 
         retranslateUi(GuiApplicationClass);
